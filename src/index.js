2751e31be94c6a70e3983e67c650a78914e6cbec
Vid <vidmiljanovic@gmail.com>
2021-10-04 10:37:26 +0200

    Day 20-21

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const chars = document.getElementById("char");
+const space = document.getElementById("space");
+const backspace = document.getElementById("backspace");
+const caps = document.getElementById("caps");
+const shift = document.getElementById("shift");
+const enter = document. getElementById("enter");
+const ok = document.getElementById("ok");
+
+
